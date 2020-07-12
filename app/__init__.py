accd2d641b711d4803d7aeb282728fd48a61edbc
Albert Obwoge <albertnyambegaa@gmail.com>
2020-07-12 16:45:16 +0300

    added boostrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
+
 from app.models import *
 from app.views import *
