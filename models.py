4f12736c2a12e521533f25ddba8a7bdc53d8b495
Albert Obwoge <albertnyambegaa@gmail.com>
2020-07-12 12:43:54 +0300

    added class pitch

---
+++ models.py
@@ -0,0 +1,25 @@
+from . import db
+
+#...
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    lastname = db.Column(db.String(255))
+    firstname = db.Column(db.String(255))
+    pitches = db.relationship('Pitch',backref = 'owner',lazy="dynamic")
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Pitch(db.Model):
+    __tablename__ = 'pitch'
+    id = db.Column(db.Integer,primary_key = True)
+    filepath = db.Column(db.String(255))
+    owner_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    # pitch = Pitch()
+    # you have access to pitch.owner
+
+
+    def __repr__(self):
+        return f'User {self.username}'
